40c6f54a738395803bcce83335818aa0f421e70a
Gustavo de Oliveira <guxtavodeoliveira@gmail.com>
2026-02-11 21:23:14 -0300

    Add files via upload
    Envio dos arquivos finalizados HTML, CSS, JAVASCRIPT

---
+++ script.js
@@ -0,0 +1,11 @@
+const links = document.querySelectorAll('a[href^="#"]');
+
+links.forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const destino = document.querySelector(this.getAttribute('href'));
+        destino.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
